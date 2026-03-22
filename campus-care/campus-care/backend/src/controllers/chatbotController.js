a0cae396579afb17399ee8193ef67bd561668a76
aditiii0408 <aditishanbhag0408@gmail.com>
2026-03-22 21:55:32 +0530

    Fix gemini model name

--- campus-care/campus-care/backend/src/controllers/chatbotController.js
+++ campus-care/campus-care/backend/src/controllers/chatbotController.js
@@ -73,7 +73,7 @@ and STRONGLY encourage them to contact a counsellor or call iCall helpline at 91
     const apiKey = process.env.GEMINI_API_KEY;
     const options = {
       hostname: "generativelanguage.googleapis.com",
-      path: `/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`,
+      path: `/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
       method: "POST",
       headers: {
         "Content-Type": "application/json",
